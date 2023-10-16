dbf8ddc138af362a76574e30e01f2cd227dbd118
zihadhossion <zihadhossion2005@gmail.com>
2023-10-16 22:05:35 +0600

    sidebar update

--- main.js
+++ main.js
@@ -9,13 +9,26 @@ let swiper2 = new Swiper(".heroSwiper", {
   },
 });
 
+// //search appear
+const modal = document.getElementById("myModal");
+const closeModal = document.querySelector("#myModal .close");
+const btn = document.getElementById("myBtn");
+btn.addEventListener("click", () => {
+  modal.classList.add("show-modal")
+})
+
+closeModal.addEventListener("click", () => {
+  modal.classList.remove("show-modal");
+})
+
+
 //mobile menu bar toggle 
 const menuBtn = document.getElementById("menu-bar");
 const closeMenubar = document.getElementById("close-bar");
 const mobSidebar = document.querySelector(".mob-sidebar");
 
 function toggleMenu() {
-  mobSidebar.classList.toggle("hidden");
+  mobSidebar.classList.add("hidden");
   let boolValue = false;
   if (mobSidebar.classList.contains("hidden")) {
     boolValue = boolValue;
@@ -25,9 +38,11 @@ function toggleMenu() {
     document.getElementsByTagName("body")[0].style.overflow = boolValue ? "hidden" : "";
   }
 }
-
 menuBtn.addEventListener("click", toggleMenu);
-closeMenubar.addEventListener("click", toggleMenu);
+
+closeMenubar.addEventListener("click", () => {
+  mobSidebar.classList.remove("hidden");
+});
 
 //sticky bar for window scroll
 window.addEventListener("scroll", () => {
@@ -46,42 +61,30 @@ window.addEventListener("scroll", () => {
 })
 
 //toggle for sub menu item
-const aeroRights = document.querySelectorAll(".mob-sidebar .fa-chevron-right");
 const backAero = document.querySelector(".mob-sidebar .fa-chevron-left");
 const mainMenus = document.querySelector(".mob-sidebar .navlinks");
 
-for (const item of aeroRights) {
+function showSubMenu(step) {
+  const ele = document.querySelector(`.submenu${step}`);
+  console.log(ele);
+  ele.classList.toggle("hidden");
+  mainMenus.style.transform = "translate(100%)";
+  ele.parentElement.style.transform = "translate(10%)";
 
-  function showSubMenu(step) {
-    const ele = document.querySelector(`.submenu${step}`);
-    console.log(ele);
-
-    item.addEventListener("click", () => {
-      ele.classList.remove("hidden");
-      mainMenus.style.transform = "translate(100%)";
-      ele.parentElement.style.transform = "translate(10%)";
-    });
-
-    backAero.addEventListener("click", () => {
-      ele.classList.add("hidden");
-      ele.parentElement.style.transform = "translate(100%)";
-      mainMenus.style.transform = "translate(0%)";
-    })
-  }
+  backAero.addEventListener("click", () => {
+    ele.classList.add("hidden");
+    ele.parentElement.style.transform = "translate(100%)";
+    mainMenus.style.transform = "translate(0%)";
+  })
 }
 
 //browse category
-
 const browseMenu = document.querySelector(".browse");
 const browseCate = document.querySelector(".browse-category");
 
 browseMenu.addEventListener("click", () => {
   browseCate.classList.toggle("tBrow")
 })
-
-
-
-
 
 //slider Categories item
 let swiper = new Swiper(".mySwiper", {
